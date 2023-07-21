868acaffaeb363ad0414b1ede46452c0f69fea35
iosif0706 <117926524+iosif0706@users.noreply.github.com>
2023-07-21 06:02:49 +0000

    gg

--- python/searchelementhtml.py
+++ python/searchelementhtml.py
@@ -2,27 +2,32 @@
 def checkelement(element):
     
     found = []
-    letter = ''
-    element = ''
+    count = 0
+    word = ''
+    bool = True
+    open_element = ''
+    close_element = ''
     for check in html:
-            letter += check
-            if letter[0] == '<' and letter[-1] == '>':
-                found.append(letter)
-                letter = ''
-            elif letter[0] != '<':
-                letter = ''
+            word += check
+            if word == '<div>' or word == '<p>' or word == '<i>' or word == '<a>':
+                open_element += word
+                count += 1
+                word = ''
+            elif  word == '</div>' or word == '</p>' or word == '</i>' or word == '</a>':
+                open_element += word
+                count -= 1
+                word = ''
+            elif word[0] != '<':
+                word = ''
+     
                 
-    open_element = []
-    close_element = []
-    for item in found:
-        if item == '<div>' or item == '<p>' or item == '<i>' or item == '<a>':
-            open_element.append(item)
-        elif item == '</div>' or item == '</p>' or item == '</i>' or item == '</a>':
-            close_element.append(item)
-    
+
+                
+    if count == 1:
+        bool = False
     print(open_element)
     print(close_element)
-    print(True)
+    print(bool)
         
         
 
